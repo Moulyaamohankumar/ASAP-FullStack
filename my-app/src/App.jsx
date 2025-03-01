f891d8eace7fc1d064e94ebec73a522ac0bb8d87
Moulyaamohankumar <moulyaa2626@gmail.com>
2025-03-01 09:22:05 +0530

    done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Task from "./components/Task";
+
+function App() {
+  return (
+    <div>
+      <h1>ASAP Project</h1>
+      <Task title="Test Task" description="This is a dummy task" status="Pending" />
+    </div>
+  );
+}
+
+export default App;
